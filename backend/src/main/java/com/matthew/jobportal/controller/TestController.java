c679393bf65ab9374f3b47382d98943c8586da50
Matthew Dsouza <matthewdsouza.one@outlook.com>
2026-04-20 02:19:56 +0530

    chore: move sensitive configs to environment variables

---
+++ backend/src/main/java/com/matthew/jobportal/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.matthew.jobportal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test/health")
+    public String healthCheck() {
+        return "Backend is running";
+    }
+}
